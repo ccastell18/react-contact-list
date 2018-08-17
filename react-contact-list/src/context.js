0ab3c696c445ba9e8a21268d92428b83cb69884c
Christopher Castellano <c.castellano18@gmail.com>
2018-08-17 17:14:44 -0500

    axios

--- react-contact-list/src/context.js
+++ react-contact-list/src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -23,28 +24,15 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '444-444-4444'
-      },
-      {
-        id: 2,
-        name: 'Jane Doe',
-        email: 'janedoe@gmail.com',
-        phone: '444-444-5555'
-      },
-      {
-        id: 3,
-        name: 'Bob Doe',
-        email: 'Bdoe@gmail.com',
-        phone: '444-555-4444'
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => this.setState({ contacts: res.data }));
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
